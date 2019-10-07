e6b9f19d526d24844f3d912dd2a154452694bd96
ingatrifan <ingaa.trifan@gmail.com>
2019-10-07 21:33:56 +0300

    mLab commit

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var blogRoutes = require('./routes/blogs'),
     commentRoutes = require('./routes/comments'),
     userRoutes    = require('./routes/users');
 
-mongoose.connect('mongodb://localhost:27017/infoblog' , { useNewUrlParser: true } );
+mongoose.connect('mongodb+srv://Blog:assist@infoblog-yusiw.mongodb.net/admin?retryWrites=true&w=majority')
+// mongoose.connect('mongodb://localhost:27017/infoblog' , { useNewUrlParser: true } );
 
 app.set("view engine","ejs");
 app.use(express.static("public"));
